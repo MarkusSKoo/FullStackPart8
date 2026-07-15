6a8926ebd67cfe7c89d26ae8a2fcefb747a4dfd4
MarkusSKoo <markus.kauhanen@helsinki.fi>
2026-07-15 12:29:26 +0300

    Submission: Part 8 chapter 3

--- library-frontend/src/App.jsx
+++ library-frontend/src/App.jsx
@@ -1,26 +1,49 @@
-import { useState } from 'react'
-import Authors from './components/Authors'
-import Books from './components/Books'
-import NewBook from './components/NewBook'
+import { useState } from "react";
+import Authors from "./components/Authors";
+import Books from "./components/Books";
+import NewBook from "./components/NewBook";
+import { useQuery } from "@apollo/client/react";
+import { ALL_AUTHORS, ALL_BOOKS } from "./queries";
+import Notify from "./components/Notify";
 
 const App = () => {
-  const [page, setPage] = useState('authors')
+  const [page, setPage] = useState("authors");
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const authorsResult = useQuery(ALL_AUTHORS);
+  const booksResult = useQuery(ALL_BOOKS);
+  if (authorsResult.loading || booksResult.loading) {
+    return <div>...loading</div>;
+  }
+
+  const notify = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 10000);
+  };
 
   return (
     <div>
       <div>
-        <button onClick={() => setPage('authors')}>authors</button>
-        <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
+        <button onClick={() => setPage("authors")}>authors</button>
+        <button onClick={() => setPage("books")}>books</button>
+        <button onClick={() => setPage("add")}>add book</button>
       </div>
 
-      <Authors show={page === 'authors'} />
+      <Notify errorMessage={errorMessage} />
 
-      <Books show={page === 'books'} />
+      <Authors
+        show={page === "authors"}
+        authors={authorsResult.data.allAuthors}
+        setError={notify}
+      />
 
-      <NewBook show={page === 'add'} />
+      <Books show={page === "books"} books={booksResult.data.allBooks} />
+
+      <NewBook show={page === "add"} setError={notify} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- library-frontend/src/components/Authors.jsx
+++ library-frontend/src/components/Authors.jsx
@@ -1,30 +1,89 @@
+import { useState } from "react";
+import { useMutation } from "@apollo/client/react";
+import { UPDATE_BORN, ALL_AUTHORS } from "../queries";
+
 const Authors = (props) => {
+  const [name, setName] = useState("");
+  const [born, setBorn] = useState("");
+  const setError = props.setError;
+
+  const [updateBorn] = useMutation(UPDATE_BORN, {
+    onCompleted: (data) => {
+      if (!data.editAuthor) {
+        setError("Author not found");
+      }
+    },
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  });
+
   if (!props.show) {
-    return null
+    return null;
   }
-  const authors = []
+
+  const authors = props.authors;
+
+  const submit = async (event) => {
+    event.preventDefault();
+
+    await updateBorn({
+      variables: { name, setBornTo: Number(born) },
+    });
+
+    setName("");
+    setBorn("");
+  };
 
   return (
     <div>
-      <h2>authors</h2>
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>born</th>
-            <th>books</th>
-          </tr>
-          {authors.map((a) => (
-            <tr key={a.id}>
-              <td>{a.name}</td>
-              <td>{a.born}</td>
-              <td>{a.bookCount}</td>
+      <div>
+        <h2>authors</h2>
+        <table>
+          <tbody>
+            <tr>
+              <th></th>
+              <th>born</th>
+              <th>books</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+            {authors.map((a) => (
+              <tr key={a.id}>
+                <td>{a.name}</td>
+                <td>{a.born}</td>
+                <td>{a.bookCount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <form onSubmit={submit}>
+        <h3>Set birthyear</h3>
+        <div>
+          name
+          <select
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+            required
+          >
+            <option value="" disabled>
+              Select author
+            </option>
+            {authors.map((a) => (
+              <option key={a.id} value={a.name}>
+                {a.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          born
+          <input
+            value={born}
+            onChange={({ target }) => setBorn(target.value)}
+          ></input>
+        </div>
+        <button type="submit">update author</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Authors
+export default Authors;
